cfb21b0ae69baa2adbfa538fb224bc0756249572
cs-shubham-morankar <smorankar@fortinet.com>
2023-11-01 14:08:37 +0530

    Updated connector name

--- arbor-aed/constants.py
+++ arbor-aed/constants.py
@@ -1,8 +1,9 @@
-""" Copyright start
-  Copyright (C) 2008 - 2023 Fortinet Inc.
-  All rights reserved.
-  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
-  Copyright end """
+"""
+Copyright start
+MIT License
+Copyright (c) 2023 Fortinet Inc
+Copyright end
+"""
 
 PARAM_MAP = {'Ascending': 'ASC', 'Descending': 'DESC', 'Active': 1, 'Inactive': 0, 'None': None, 'Low': 1, 'Medium': 2,
              'High': 3, True: 1, False: 0}
